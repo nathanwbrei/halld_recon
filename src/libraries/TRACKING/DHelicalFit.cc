98932c0976b11424e7e7d2784f24311b3f66e38d
Simon Taylor <staylor@ifarm1801.jlab.org>
2019-06-05 17:51:27 -0400

    comment out debug text

--- src/libraries/TRACKING/DHelicalFit.cc
+++ src/libraries/TRACKING/DHelicalFit.cc
@@ -286,8 +286,8 @@ jerror_t DHelicalFit::AddStereoHit(const DCDCWire *wire){
   // distance along wire relative to origin
   double s=dz/cos(wire->stereo);
   
-  //  if(DEBUG_LEVEL>15)
-    _DBG_<<"s="<<s<<" ring="<<wire->ring<<" straw="<<wire->straw<<" stereo="<<wire->stereo<<endl;
+    //if(DEBUG_LEVEL>15)
+    //_DBG_<<"s="<<s<<" ring="<<wire->ring<<" straw="<<wire->straw<<" stereo="<<wire->stereo<<endl;
   if(fabs(s) > 0.5*wire->L) return VALUE_OUT_OF_RANGE; // if wire doesn't cross circle, skip hit
 		
   // Compute the position for this hit
